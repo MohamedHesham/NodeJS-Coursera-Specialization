317102aed1423bfe8f86505c90793925286f456c
Mohamed Hesham Alma'zon <m.hesham@outlook.com>
2018-12-27 10:52:50 +0800

    Assignment 1: Node Modules, Express, and REST API

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 const dishRouter = require('./routes/dishRouter');
+const promotionsRouter = require('./routes/promotionsRouter');
+const leadersRouter = require('./routes/leadersRouter');
+
 
 const hostname = 'localhost';
 const port = 3000;
@@ -13,6 +16,8 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 
 app.use('/dishes', dishRouter);
+app.use('/promotions', promotionsRouter);
+app.use('/leaders', leadersRouter);
 app.use(express.static(__dirname+'/public'));
 
 app.use((req, res, next) => {
